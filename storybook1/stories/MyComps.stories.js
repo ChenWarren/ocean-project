30125fb218cba72abfefb7336eb179924c6eea6a
MaharlikaC <mclomata@my.bcit.ca>
2021-03-04 13:25:31 -0800

    my 2 comps

---
+++ storybook1/stories/MyComps.stories.js
@@ -0,0 +1,10 @@
+import {Button} from '../comps/Button/index.js'
+import {DelObject} from '../comps/RemovableObject/index.js'
+
+export default {
+  title: "My Components"
+}
+
+
+export const MyButton = () => Button;
+export const MyDelObject = () => DelObject;
---
+++ storybook1/workflow.js
@@ -0,0 +1,2 @@
+document.querySelector(".main").innerHTML += ButtonUI();
+document.querySelector(".main").innerHTML += DelObjectUI('https://placekitten.com/200/200', 200, 200, 50);
