6c87cfbe18fcf18befcd65fc06c46c544430d33e
ChenWarren <hchen273@my.bcit.ca>
2021-03-05 10:46:18 -0800

    animation added to landing page

--- comps/webname/index.js
+++ comps/webname/index.js
@@ -1,7 +1,12 @@
 function webName(web_name)
 {
-    return `<h1 style='
+    return `<h1 id='webname' style='
+        color: #FFF;
         text-shadow: 0px 2px 2px rgba(0, 0, 0, 0.25);
+        position: relative;
+        animation-name: topIn;
+        animation-duration: 1.5s;
+        animation-fill-mode: forwards;
     '>${web_name}</h1>
     `
 }
--- js/comps.js
+++ js/comps.js
@@ -1,7 +1,8 @@
 document.querySelector('#menu').innerHTML += Menu();
 
-document.querySelector('#webname').innerHTML += webName('PeaceOcean');
+document.querySelector('.landing').innerHTML += webName('PeaceOcean');
 
-document.querySelector('.landing').innerHTML += category('Importance', 'Ocean is so important to us');
-document.querySelector('.landing').innerHTML += category('Threats', 'to the oceans');
-document.querySelector('.landing').innerHTML += category('Protection', 'The things that we can do');
+document.querySelector('.landing').innerHTML += category('Importance', 'Ocean is so important to us','rightIn');
+document.querySelector('.landing').innerHTML += category('Threats', 'to the oceans','leftIn');
+document.querySelector('.landing').innerHTML += category('Protection', 'The things that we can do','rightIn');
+
